128ec569c5781c0d98447584426b0c2683acf049
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-08-31 18:11:58 +0530

    [roshan]fibonaci series

--- Day6Assignment/Program.cs
+++ Day6Assignment/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("welecome to fobonaci series ");
+            Console.WriteLine("chooes program\n1. fibonaci series");
+            int opt=Convert.ToInt32(Console.ReadLine());
+
+            switch(opt)
+            {
+                case 1:fibonaciSeries.FibonaciSeries();
+                    
+                    break;
+            }
         }
     }
 }
