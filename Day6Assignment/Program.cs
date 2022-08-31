291e8c967d4ec27925a4c15520d72755639cf0ba
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-08-31 22:21:05 +0530

    [roshan]day of week

--- Day6Assignment/Program.cs
+++ Day6Assignment/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("welecome to fobonaci series ");
-            Console.WriteLine("chooes program\n1. fibonaci series\n2.find perfect number\n3.find prime number or not\n4.reverse a number\n5.stop watch program\n6.Find the Fewest Notes to be returned for Vending Machine");
+            Console.WriteLine("chooes program\n1. fibonaci series\n2.find perfect number\n3.find prime number or not\n4.reverse a number\n5.stop watch program\n6.Find the Fewest Notes to be returned for Vending Machine\n7.Day of week");
             int opt=Convert.ToInt32(Console.ReadLine());
 
             switch(opt)
@@ -25,6 +25,11 @@
                     break;
                 case 6:
                     vedingMachineproblem.VedingMachineProblem();
+                    break;
+                case 7:
+                    dayOffWeek.DayOfWeek();
+                    break;
+                    default:
                     break;
             }
         }
