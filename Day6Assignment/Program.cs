42993bf9ddb78b1b89583639d7160eea6a2ed4a1
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-08-31 18:33:59 +0530

    [roshan]number is perfect or not

--- Day6Assignment/Program.cs
+++ Day6Assignment/Program.cs
@@ -5,13 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("welecome to fobonaci series ");
-            Console.WriteLine("chooes program\n1. fibonaci series");
+            Console.WriteLine("chooes program\n1. fibonaci series\n2.find perfect number");
             int opt=Convert.ToInt32(Console.ReadLine());
 
             switch(opt)
             {
                 case 1:fibonaciSeries.FibonaciSeries();
-                    
+                   break;
+                    case 2:perfectNumber.PerfectNumber();
                     break;
             }
         }
