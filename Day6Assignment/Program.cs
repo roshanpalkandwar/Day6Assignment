3573e561a2332bb94b7a820e914def37d9b396aa
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-08-31 15:55:30 +0530

    [roshan]welcome msg

---
+++ Day6Assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day6Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welecome to fobonaci series ");
+        }
+    }
+}
