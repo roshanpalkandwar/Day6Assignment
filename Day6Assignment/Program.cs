ecfada078b45b7a05b8ab884425e850c71b118ae
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-02 00:02:32 +0530

    [roshan]sqr root program

--- Day6Assignment/Program.cs
+++ Day6Assignment/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("welecome to fobonaci series ");
-            Console.WriteLine("chooes program\n1. fibonaci series\n2.find perfect number\n3.find prime number or not\n4.reverse a number\n5.stop watch program\n6.Find the Fewest Notes to be returned for Vending Machine\n7.Day of week\n8.monthly payment\n9.tempreture converision\n10.to binary\n11.coupan Number");
+            Console.WriteLine("chooes program\n1. fibonaci series\n2.find perfect number\n3.find prime number or not\n4.reverse a number\n5.stop watch program\n6.Find the Fewest Notes to be returned for Vending Machine\n7.Day of week\n8.monthly payment\n9.tempreture converision\n10.to binary\n11.coupan Number\n12.sqrt root program");
             int opt=Convert.ToInt32(Console.ReadLine());
 
             switch(opt)
@@ -42,6 +42,9 @@
                     break;
                 case 11:
                   coupanNumber.Coupon();
+                    break;
+                case 12:
+                    Sqrt.squareRoot();
                     break;
             }
         }
